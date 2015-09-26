a28a7854cebc179c25c5803753ac1523c02918eb
Michaël Fortin <michael@simbioz.com>
2015-09-26 13:47:56 -0400

    Add "build in progress" animation.

---
+++ src/BuildScreen/Converters/EqualToOneToHiddenOrVisibleConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace BuildScreen.Converters
+{
+    public class EqualToOneToHiddenOrVisibleConverter : OneWayValueConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Math.Abs((double)value - 1.0) < 0.00001 ? Visibility.Hidden : Visibility.Visible;
+        }
+    }
+}
---
+++ src/BuildScreen/Converters/EqualToOneToVisibleOrHiddenConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace BuildScreen.Converters
+{
+    public class EqualToOneToVisibleOrHiddenConverter : OneWayValueConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Math.Abs((double)value - 1.0) < 0.00001 ? Visibility.Visible : Visibility.Hidden;
+        }
+    }
+}
